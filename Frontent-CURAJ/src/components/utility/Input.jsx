a226ea53c993515927b9c81d74eda95c25585956
WebProject720 <rameshmalisundesha720@gmail.com>
2025-01-26 23:45:45 +0530

    auth basic pages

--- Frontent-CURAJ/src/components/utility/Input.jsx
+++ Frontent-CURAJ/src/components/utility/Input.jsx
@@ -4,17 +4,25 @@ export const Input = (props) => {
             <label htmlFor={props.id} className="font-bold">
                 {props.label}
             </label>
-            <input
-                className={`bg-gray-100 border-gray-400 outline-none border-[2px] 
-                    rounded-full p-2 
+            <div className="flex flex-row group w-full gap-0 flex-nowrap items-center ">
+                <input
+                    className={`bg-gray-100 border-gray-400 outline-none border-[2px] 
+                    rounded-full p-2  
                     focus:bg-gray-200 focus:border-blue-800 focus:shadow-md focus:shadow-blue-200
-                    transition-all duration-500
-                     ${props.className}`}
-                type={props.type}
-                placeholder={props.placeholder}
-                name={props.name}
-                id={props.id}
-            />
+                    transition-all duration-500 
+                     ${props.className} ${props.text ? 'w-4/6 rounded-r-none' : 'w-full'}`}
+                    type={props.type}
+                    placeholder={props.placeholder}
+                    name={props.name}
+                    id={props.id}
+                />
+                <div className={`rounded-r-full border-l-0 border-gray-400 border-[2px] p-2 hover:cursor-not-allowed 
+                bg-gray-200
+                    text-gray-500 font-bold
+                    ${props.text ? 'w-2/6' : 'hidden'}`}>
+                    {props.text}
+                </div>
+            </div>
         </div>
     )
 }
