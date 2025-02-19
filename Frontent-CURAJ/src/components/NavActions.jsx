0d4664e09f7eab1ba9ac4de1abb3cfc13afb0138
WebProject720 <rameshmalisundesha720@gmail.com>
2025-02-19 16:50:53 +0530

    logout loader

--- Frontent-CURAJ/src/components/NavActions.jsx
+++ Frontent-CURAJ/src/components/NavActions.jsx
@@ -3,10 +3,13 @@ import { Button } from "./utility/Button";
 import { getStoreData } from "../store";
 import { logoutUser } from "../APIs/auth/logout";
 import { useEffect, useState } from "react";
+import { Loader } from "./utility/Loader";
+
 
 export const NavActions = (props) => {
   const [store, setStore] = useState(null);
   const [user, setUser] = useState(null);
+  const [loading, SetLoading] = useState(false);
   useEffect(() => {
     // if (typeof window !== undefined) {
     const storeData = getStoreData();
@@ -14,6 +17,11 @@ export const NavActions = (props) => {
     setUser(storeData.loggedUser);
     // }
   }, [])
+
+  const logout = () => {
+    logoutUser();
+    SetLoading(true);
+  }
 
   return (
     <div className="font-bold">
@@ -23,23 +31,12 @@ export const NavActions = (props) => {
         ) : (
           <div className="flex items-center justify-center gap-2">
             <div className="">
-              <Button onClick={logoutUser}>Logout</Button>
-            </div>
-            <div className="phone:hidden flex flex-row">
-              <div className="flex flex-row cursor-pointer group gap-2 items-center justify-center bg-transparent p-2 rounded-md">
-                <div>
-                  <img
-                    src={Images?.userIcon}
-                    className="desktop:w-10 w-20"
-                  />
-                </div>
-              </div>
-              <div className="flex flex-col items-start gap-0">
-                <h1>
-                  <center>{user?.username || "Username"}</center>
-                </h1>
-                <p className="text-sm font-light">{user?.email || ""}</p>
-              </div>
+              <Button onClick={logout}>
+                {
+                  loading ? <Loader></Loader> :
+                    Logout
+                }
+              </Button>
             </div>
           </div>
         )
