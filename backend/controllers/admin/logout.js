747221840faad65c1967b035fb6f3e2311bd5810
Zero1Byte <rameshmalisundesha720@gmail.com>
2025-04-22 19:19:09 +0530

    Server logout Bug

--- backend/controllers/admin/logout.js
+++ backend/controllers/admin/logout.js
@@ -1,6 +1,6 @@
 import { ApiError } from "../../utils/apiError.js";
 import { ApiResponse } from "../../utils/apiResponse.js";
-import 'dotenv/config'
+import 'dotenv/config';
 import { CookieOption } from "../../utils/cookieOptions.js";
 import jsonwebtoken from "jsonwebtoken";
 
@@ -12,32 +12,44 @@ export const logoutAdmin = async (req, res) => {
         const TokenName = process.env.AdminTokenName || 'AdminToken';
         const cookie = req?.cookies[TokenName];
 
-
-        if (!_id && cookie) {
+        // Check if the cookie exists
+        if (!cookie) {
             return res.status(400).json(
-                new ApiError('Session not found')
-            )
+                new ApiError('Token not provided', null, false, 400)
+            );
+        }
+
+        if (!_id) {
+            return res.status(400).json(
+                new ApiError('Session not found', null, false, 400)
+            );
         }
 
         const user = jsonwebtoken.verify(cookie, process.env.JWT_SECRET_KEY);
         if (user._id != _id) {
             return res.status(400).json(
-                new ApiError('User session ID not matched !')
-            )
+                new ApiError('User session ID not matched!', null, false, 400)
+            );
         }
 
         return res
             .status(200)
             .clearCookie(TokenName, CookieOptions)
             .json(
-                new ApiResponse('Admin logout')
-            )
+                new ApiResponse('Admin logout', null, true, 200)
+            );
 
     } catch (error) {
-        console.log(error);
+        console.error(error);
+
+        if (error.name === "JsonWebTokenError") {
+            return res.status(401).json(
+                new ApiError('Invalid token', error.message, false, 401)
+            );
+        }
 
         return res.status(500).json(
-            new ApiError('Server Error', error, false)
-        )
+            new ApiError('Server Error', error.message, false, 500)
+        );
     }
-}
+};
